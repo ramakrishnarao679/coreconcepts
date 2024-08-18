23be599f4368f9d92ecc2b6b8050e3814de52e5c
ramakrishnarao679 <ramakrishnarao679@gmail.com>
2024-08-18 09:23:41 +0530

    incrementAsync added

--- coreconcepts/src/App.js
+++ coreconcepts/src/App.js
@@ -6,9 +6,16 @@ function App () {
   const increment = ()=>{
     setNumbers(numbers+5);
   }
+
+  const incrementAsync = ()=>{
+    setTimeout(()=>{
+      setNumbers((currentNumber)=>currentNumber+10)
+    }, 2000);
+  }
   return (
     <div>
       <button onClick={increment}>Increment</button>
+      <button onClick={incrementAsync}>IncrementAsync</button>
       <h2>{numbers}</h2>
     </div>
   )
